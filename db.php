4820cec61a94a7f78de5ee42b53613a44b14d4a3
Igor <admin@aferon.com>
2020-12-08 19:17:15 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqlLink = mysqli_connect("home.aferon.com", "login", "qwe123!!");
+$mysqlLink = mysqli_connect("home.aferon.com", "login", "");
 mysqli_select_db($mysqlLink, "karma8_inc");
 mysqli_query($mysqlLink, "SET NAMES utf8");
 
